90c9ac512e5f69893ba3190ee2e05241ff1fac59
bob <bob@qq.com>
2022-11-08 09:51:30 +0800

    二刷

---
+++ build/CMakeFiles/work_manager.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/bob/workspace/CPPProjects/cpp_study/Boss.cpp" "CMakeFiles/work_manager.dir/Boss.cpp.o" "gcc" "CMakeFiles/work_manager.dir/Boss.cpp.o.d"
+  "/Users/bob/workspace/CPPProjects/cpp_study/Employee.cpp" "CMakeFiles/work_manager.dir/Employee.cpp.o" "gcc" "CMakeFiles/work_manager.dir/Employee.cpp.o.d"
+  "/Users/bob/workspace/CPPProjects/cpp_study/Manager.cpp" "CMakeFiles/work_manager.dir/Manager.cpp.o" "gcc" "CMakeFiles/work_manager.dir/Manager.cpp.o.d"
+  "/Users/bob/workspace/CPPProjects/cpp_study/main.cpp" "CMakeFiles/work_manager.dir/main.cpp.o" "gcc" "CMakeFiles/work_manager.dir/main.cpp.o.d"
+  "/Users/bob/workspace/CPPProjects/cpp_study/workManager.cpp" "CMakeFiles/work_manager.dir/workManager.cpp.o" "gcc" "CMakeFiles/work_manager.dir/workManager.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/work_manager.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/work_manager.dir/Boss.cpp.o"
+  "CMakeFiles/work_manager.dir/Boss.cpp.o.d"
+  "CMakeFiles/work_manager.dir/Employee.cpp.o"
+  "CMakeFiles/work_manager.dir/Employee.cpp.o.d"
+  "CMakeFiles/work_manager.dir/Manager.cpp.o"
+  "CMakeFiles/work_manager.dir/Manager.cpp.o.d"
+  "CMakeFiles/work_manager.dir/main.cpp.o"
+  "CMakeFiles/work_manager.dir/main.cpp.o.d"
+  "CMakeFiles/work_manager.dir/workManager.cpp.o"
+  "CMakeFiles/work_manager.dir/workManager.cpp.o.d"
+  "work_manager"
+  "work_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/work_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
